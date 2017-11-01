9749a3b3a8cd8b28abbf0e7f29649b01bf519795
MariannaMilovanova <mmilovanova@kse.org.ua>
2017-11-01 12:23:51 +0200

    add music to git ignore

--- src/components/audioplayer.js
+++ src/components/audioplayer.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import ReactAudioPlayer from 'react-audio-player';
 
 const playlist = [
-    {url: '../music/06_-_Jailhouse_Rock.mp3', artist: {name: 'Metallica2', song: 'Fuel1'}},
-    {url: '../music/Cellophane_Sam_-_02_-_Birds_at_Sea.mp3', artist: {name: 'Metallica3', song: 'Fuel'}},
-    {url: '../music/Rafael_Archangel_-_21_-_Persistence.mp3', artist: {name: 'Metallica4', song: 'Fuel'}}
+    {url: '../music/06_-_Jailhouse_Rock.mp3', artist: {name: 'Metallica2', song: 'Fuel1'}}
 ]
 
 class AudioPlayer extends Component {
